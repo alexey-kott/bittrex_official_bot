2226c607cbbbb309d30e0e5c2afe545ea6607ecb
Alexey Kott <alexey.kott@gmail.com>
2017-11-02 15:49:36 +0300

    bot.polling в цикле

--- bot.py
+++ bot.py
@@ -334,4 +334,9 @@ if __name__ == '__main__':
 	watcher = Watcher()
 	w = Process(target = watcher)
 	w.start()
-	bot.polling(none_stop=True)
+	while True:
+		try:
+			bot.polling(none_stop=True)
+		except Exception as e:
+			print(e)
+			sleep(3.5)
